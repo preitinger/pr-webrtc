5dace31b5be2865b60a565ce94d9efcb3300a608
preitinger <peter.reitinger@gmail.com>
2024-02-06 10:27:12 +0100

    install next-pwa as described in https://ducanh-next-pwa.vercel.app/docs/next-pwa/getting-started

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,49 @@
-import type { Metadata } from 'next'
+import type { Metadata, Viewport } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
+const APP_NAME = "pr-webrtc";
+const APP_DEFAULT_TITLE = "pr-webrtc";
+const APP_TITLE_TEMPLATE = "%s - pr-webrtc";
+const APP_DESCRIPTION = "Chat and video calls";
+
 export const metadata: Metadata = {
-  title: 'pr-webRTC',
-  description: 'Simpler Chat und Internet-Videoanrufe',
-  manifest: "/manifestx.json", // we are accessing our manifest file here
+
+    applicationName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+    manifest: "/manifest.json",
+    appleWebApp: {
+      capable: true,
+      statusBarStyle: "default",
+      title: APP_DEFAULT_TITLE,
+      // startUpImage: [],
+    },
+    formatDetection: {
+      telephone: false,
+    },
+    openGraph: {
+      type: "website",
+      siteName: APP_NAME,
+      title: {
+        default: APP_DEFAULT_TITLE,
+        template: APP_TITLE_TEMPLATE,
+      },
+      description: APP_DESCRIPTION,
+    },
+    twitter: {
+      card: "summary",
+      title: {
+        default: APP_DEFAULT_TITLE,
+        template: APP_TITLE_TEMPLATE,
+      },
+      description: APP_DESCRIPTION,
+    },    
 }
 
 export default function RootLayout({
--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// @ts-check
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // TODO ? config if necessary
+}
+
+const withPWA = require("@ducanh2912/next-pwa").default({
+    dest: "public",
+});
+
+module.exports = withPWA(nextConfig)
+
+// module.exports = nextConfig
