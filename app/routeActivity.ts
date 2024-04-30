20f87110a3b74520baa957f9db250695e290facd
preitinger <peter.reitinger@gmail.com>
2024-04-30 09:01:22 +0200

    just to trigger build on vercel

--- app/routeActivity.ts
+++ app/routeActivity.ts
@@ -1207,6 +1207,7 @@ export default async function routeActivity(chatId: string, routeActivitySignal:
                 console.error(reason);
             }
         });
+        
         console.log('nach manageFetching');
 
         {
